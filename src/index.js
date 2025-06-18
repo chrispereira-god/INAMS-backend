3d49e8a96395215506f76b1c9f0b3b1efd87a9dd
Lisban Gonsalves <lisbanrobertgonsalves@gmail.com>
2025-06-19 00:08:14 +0530

    fixed the CORS error

--- src/index.js
+++ src/index.js
@@ -11,8 +11,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['https://inams-website.onrender.com'],
-  credentials: true
+  origin: ['https://inams-website.onrender.com', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
